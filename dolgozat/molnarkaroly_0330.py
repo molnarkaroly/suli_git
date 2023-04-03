36b7badbf0cbeb238e4c9886f0c845250fa22609
Molnár Károly <118881658+molnarkaroly0@users.noreply.github.com>
2023-04-03 15:31:19 +0200

    anyád

--- dolgozat/molnarkaroly_0330.py
+++ dolgozat/molnarkaroly_0330.py
@@ -10,13 +10,37 @@ class Telepules:
         self.lakások = int(lakások)
 
 telepulesek = []
-with open("/workspaces/suli/dolgozat/telepules.txt", "r") as f:
-  for sor in f.read().splitlines()[1:]:
+with open("telepules.txt", "r", encoding="utf-8") as f:
+  for sor in f.read().splitlines()[0:]:
     telepulesek.append(Telepules(sor))
 
+# Hány város rangú település található?
 db = 0
 for e in telepulesek:
-   if e.rang == "város":
+   if e.rangja == "város":
       db += 1
 
-print(db)
+print(f'\n{db} város rangú település található?')
+
+#Van-e falu rangú település?
+van = False
+for e in telepulesek:
+    if e.rangja == "falu":
+        van = True
+        break
+print(f'\n{"Van " if van else "Nincs"} falu rangú település')
+print('\n')
+#Írja ki a község rangú települések közül az 1000 főnél népesebb települések nevét és népességét!
+
+for e in telepulesek:
+    if e.rangja == "község" and e.népessége > 1000:
+        print(f'{e.neve} és a népessége {e.népessége} fő')
+        
+#Igaz-e, hogy minden Kisteleki kistérségű település lélekszáma kisebb, mint 10000?
+van = True
+for e in telepulesek:
+    if e.besorolás == "Kisteleki" and e.népessége > 10000:
+        van = False
+        break
+
+print(f'\n{"Igaz" if van else "Nem igaz"} hogy minden Kisteleki kistérségű település lélekszáma kisebb, mint 10000 \n')
