df626a405b7bf7394fc71ee53ca4347d7c9a64f8
Molnár Károly <118881658+molnarkaroly@users.noreply.github.com>
2025-03-26 09:39:19 +0100

    mod_hello

---
+++ mod_hello/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,12 @@
+<?php
+namespace KissGabor\Module\Hello\Site\Dispatcher;
+
+ \defined('_JEXEC') or die;
+
+ use Joomla\CMS\Dispatcher\DispatcherInterface;
+
+ class Dispatcher implements DispatcherInterface {
+    public function dispatch() {
+        echo '<h4>Hello<\h4>';
+    }
+ }
